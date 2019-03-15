d3afc38e3d4f485b7e76370026ab3772c11698aa
Jonathan Tutterow <jatwpw@mst.edu>
2019-03-15 03:58:47 -0500

    improved mountain gen

--- map.hpp
+++ map.hpp
@@ -18,17 +18,22 @@ void Map::deleteMatrix(){
 void Map::fillMatrix(){
     for(int r = 0; r < m_rows; r++){
         for(int c = 0; c < m_cols; c++){
-            if ((c == 0) || (c == (m_cols - 1))){
-                m_data[r][c] = '|';
-            }
-            else if ((r == 0) || (r == (m_rows - 1))){
-                m_data[r][c] = '-';
-            }
-            else{
-                m_data[r][c] = ' ';
-            }
+            m_data[r][c] = ' '; //Clearing everything
         }
     }
+    //Creating border
+    for(int c = 0; c < m_cols; c++){
+        m_data[0][c] = '-';
+        m_data[(m_rows - 1)][c] = '-';
+    }
+    for(int r = 0; r < m_rows; r++){
+        m_data[r][0] = '|';
+        m_data[r][(m_cols - 1)] = '|';
+    }
+    m_data[0][0] = '+';
+    m_data[0][(m_cols - 1)] = '+';
+    m_data[(m_rows - 1)][0] = '+';
+    m_data[(m_rows - 1)][(m_cols - 1)] = '+';
 
 }
 
@@ -44,8 +49,8 @@ void Map::printMatrix(){
 void Map::placeMountains(){
     int rowLength = (rand() % 3) + 6;
     int colLength = (rand() % 3) + 6;
-    int startRow = rand() % (m_rows - 1); //Top left corner
-    int startCol = rand() % (m_cols - 1);
+    int startRow = (rand() % (m_rows - 1)) + 1; //Top left corner
+    int startCol = (rand() % (m_cols - 1)) + 1;//m_rows/cols keeps it in border, -2 + 1 keeps it off the border
 
     if(startRow + rowLength > (m_rows - 1)){
         rowLength = (m_rows - 1) - startRow;
